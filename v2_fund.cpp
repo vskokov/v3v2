990ce9df34b199756db31ed721e83e37d8177528
Vladi Skokov <vladi@skokov.net>
2017-10-09 07:17:51 -0400

    modified

--- v2_fund.cpp
+++ v2_fund.cpp
@@ -807,9 +807,9 @@ int main(void)
     elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     cerr << "target done\n" << "time " << elapsed_secs << flush  << endl;
 	
-	output(0.0,V_c);
+	//output(0.0,V_c);
 
-    outHBT(1.0, 1.0, V_c);
-    outHBT(2, 0.5, V_c);
     outHBT(3.0/2.0, 2.0/3.0, V_c);
+    outHBT(1.8, 1.0/1.8, V_c);
+    outHBT(4.0, 1.0/4.0, V_c);
 }
